e19149f67819c4c6ae35a4e1b860a4f3757e5789
sinmetal <metal.tie@gmail.com>
2017-12-25 15:14:19 +0900

    Cloud Storageに置かれたsqlを読み込める機能を追加した refs #2

--- backend/bigquery.go
+++ backend/bigquery.go
@@ -7,12 +7,10 @@ import (
 
 	"github.com/favclip/ucon"
 	"github.com/favclip/ucon/swagger"
-
-	"google.golang.org/api/bigquery/v2"
-
-	"google.golang.org/appengine/log"
-
+	"github.com/pkg/errors"
 	"golang.org/x/oauth2/google"
+	"google.golang.org/api/bigquery/v2"
+	"google.golang.org/appengine/log"
 )
 
 // BigQueryAPI is BigQuery API
@@ -36,6 +34,8 @@ type BigQueryAPIPostRequest struct {
 	DstDatasetID      string `json:"dstDatasetID"`
 	DstTableID        string `json:"dstTableID"`
 	Query             string `json:"query"`
+	QueryPathBucket   string `json:"queryPathBucket"`
+	QueryPathObject   string `json:"queryPathObject"`
 	CreateDisposition string `json:"createDisposition"`
 }
 
@@ -46,6 +46,30 @@ type BigQueryAPIPostResponse struct {
 
 // Post is BigQuery API Post Handler
 func (api *BigQueryAPI) Post(ctx context.Context, form *BigQueryAPIPostRequest) (*BigQueryAPIPostResponse, error) {
+	storageService := storageService{}
+
+	{
+		b, err := json.Marshal(form)
+		if err != nil {
+			log.Errorf(ctx, "Failed to request.Body to json: %v", err)
+			return nil, err
+		}
+		log.Infof(ctx, "request.Body=%s", string(b))
+	}
+
+	if len(form.QueryPathBucket) > 1 {
+		query, err := storageService.GetObject(ctx, form.QueryPathBucket, form.QueryPathObject)
+		if err != nil {
+			log.Errorf(ctx, "Failed to Get Object From Storage: %v", err)
+			return nil, err
+		}
+		log.Infof(ctx, "query=%s", query)
+		form.Query = query
+	}
+	if len(form.Query) < 1 {
+		return nil, errors.New("query is required")
+	}
+
 	client, err := google.DefaultClient(ctx, bigquery.BigqueryScope)
 	if err != nil {
 		log.Errorf(ctx, "Failed to create client: %v", err)
@@ -86,12 +110,14 @@ func (api *BigQueryAPI) Post(ctx context.Context, form *BigQueryAPIPostRequest) 
 		return nil, err
 	}
 
-	b, err := json.Marshal(job)
-	if err != nil {
-		log.Errorf(ctx, "Failed to response job marshal to json: %v", err)
-		return nil, err
+	{
+		b, err := json.Marshal(job)
+		if err != nil {
+			log.Errorf(ctx, "Failed to response job marshal to json: %v", err)
+			return nil, err
+		}
+		log.Infof(ctx, "%s", string(b))
 	}
-	log.Infof(ctx, "%s", string(b))
 
 	return &BigQueryAPIPostResponse{
 		JobID: job.Id,

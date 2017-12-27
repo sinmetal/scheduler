23b9ed6af3d60ca0da2c7d97a3ca1e68c7c7c289
sinmetal <metal.tie@gmail.com>
2017-12-27 11:31:54 +0900

    Schedule登録APIを作成 refs #3

--- backend/schedule_api.go
+++ backend/schedule_api.go
@@ -8,6 +8,7 @@ import (
 	"github.com/favclip/ucon/swagger"
 )
 
+// ScheduleAPI is Schedule API
 type ScheduleAPI struct{}
 
 func setupScheduleAPI(swPlugin *swagger.Plugin) {
@@ -21,6 +22,48 @@ func setupScheduleAPI(swPlugin *swagger.Plugin) {
 	hInfo.Description, hInfo.Tags = "post to schedule", []string{tag.Name}
 }
 
-func (api *ScheduleAPI) Post(ctx context.Context) {
+// ScheduleAPIPostRequest is Schedule API Post form
+type ScheduleAPIPostRequest struct {
+	ProjectID         string `json:"projectID"`
+	DstProjectID      string `json:"dstProjectID"`
+	DstDatasetID      string `json:"dstDatasetID"`
+	DstTableID        string `json:"dstTableID"`
+	Query             string `json:"query"`
+	QueryPathBucket   string `json:"queryPathBucket"`
+	QueryPathObject   string `json:"queryPathObject"`
+	CreateDisposition string `json:"createDisposition"`
+	ScheduleV1        string `json:"scheduleV1"`
+	ScheduleTimezone  string `json:"scheduleTimezone"`
+}
 
+// ScheduleAPIPostResponse is Schedule API Post response
+type ScheduleAPIPostResponse struct {
+	*Schedule
+}
+
+// Post is Schedule API Post Handler
+func (api *ScheduleAPI) Post(ctx context.Context, form *ScheduleAPIPostRequest) (*ScheduleAPIPostResponse, error) {
+	store := ScheduleStore{}
+
+	ds, err := fromContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	// TODO Validation
+	key := store.NewKey(ctx, ds)
+	s := Schedule{
+		ProjectID:       form.ProjectID,
+		DstProjectID:    form.DstProjectID,
+		DstDatasetID:    form.DstDatasetID,
+		Query:           form.Query,
+		QueryPathBucket: form.QueryPathBucket,
+		QueryPathObject: form.QueryPathObject,
+		ScheduleV1:      form.ScheduleV1,
+	}
+	ss, err := store.Put(ctx, key, &s)
+	if err != nil {
+		return nil, err
+	}
+	return &ScheduleAPIPostResponse{ss}, nil
 }
--- backend/schedule_store.go
+++ backend/schedule_store.go
@@ -17,9 +17,10 @@ type Schedule struct {
 	DstProjectID    string        `json:"dstProjectID"`
 	DstDatasetID    string        `json:"dstDatasetID"`
 	DstTableID      string        `json:"dstTableID"`
-	Query           string        `json:"query"`
+	Query           string        `json:"query" datastore:",noindex"`
 	QueryPathBucket string        `json:"queryPathBucket"`
 	QueryPathObject string        `json:"queryPathObject"`
+	ScheduleV1      string        `json:"scheduleV1"`
 	CreatedAt       time.Time     `json:"createdAt"`
 	UpdatedAt       time.Time     `json:"updatedAt"`
 	SchemaVersion   int           `json:"-"`
@@ -27,14 +28,12 @@ type Schedule struct {
 
 var _ datastore.PropertyLoadSaver = &Schedule{}
 
+// Load is Load
 func (model *Schedule) Load(ctx context.Context, ps []datastore.Property) error {
-	if err := datastore.LoadStruct(ctx, model, ps); err != nil {
-		return err
-	}
-
-	return nil
+	return datastore.LoadStruct(ctx, model, ps)
 }
 
+// Save is Save
 func (model *Schedule) Save(ctx context.Context) ([]datastore.Property, error) {
 	model.SchemaVersion = 1
 	if model.CreatedAt.IsZero() {
@@ -47,14 +46,17 @@ func (model *Schedule) Save(ctx context.Context) ([]datastore.Property, error) {
 // ScheduleStore is Schedule Datastore Function
 type ScheduleStore struct{}
 
+// NewKey is Create Schedule Model Key for New Entity
 func (store *ScheduleStore) NewKey(ctx context.Context, client datastore.Client) datastore.Key {
 	return store.Key(ctx, client, uuid.New().String())
 }
 
+// Key is Create Schedule Model Key
 func (store *ScheduleStore) Key(ctx context.Context, client datastore.Client, id string) datastore.Key {
 	return client.NameKey("Schedule", uuid.New().String(), nil)
 }
 
+// Put is Schedule put to Datastore
 func (store *ScheduleStore) Put(ctx context.Context, key datastore.Key, schedule *Schedule) (*Schedule, error) {
 	client, err := fromContext(ctx)
 	if err != nil {

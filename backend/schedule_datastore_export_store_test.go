527b14440736d6ffdab6882ccdb6cdc1d4e3c55c
sinmetal <metal.tie@gmail.com>
2018-01-25 21:03:53 +0900

    Bucketを指定してScheduleDatastoreExportを取得する機能を追加 refs #22

--- backend/schedule_datastore_export_store_test.go
+++ backend/schedule_datastore_export_store_test.go
@@ -60,3 +60,48 @@ func TestScheduleDatastoreExportStore_ListAll(t *testing.T) {
 		t.Fatalf("expected ListAll results.length %d; got %d", e, g)
 	}
 }
+
+func TestScheduleDatastoreExportStore_QueryByBucket(t *testing.T) {
+	_, ctx, err := testerator.SpinUp() // gae/pythonのインスタンスが無ければ起動、あれば使いまわす
+	if err != nil {
+		t.Fatalf("%+v\n", err)
+	}
+	defer testerator.SpinDown() // プロセスをシャットダウンせずに、Datastoreなどの内容をクリアする
+
+	store := ScheduleDatastoreExportStore{}
+	client, err := fromContext(ctx)
+	if err != nil {
+		t.Fatalf("%+v\n", err)
+	}
+
+	const expectedBucket = "hoge"
+	{
+		key := store.NewKey(ctx, client)
+		_, err = store.Put(ctx, key, &ScheduleDatastoreExport{
+			Bucket: expectedBucket,
+		})
+		if err != nil {
+			t.Fatalf("%+v\n", err)
+		}
+	}
+	{
+		key := store.NewKey(ctx, client)
+		_, err = store.Put(ctx, key, &ScheduleDatastoreExport{
+			Bucket: "fuga",
+		})
+		if err != nil {
+			t.Fatalf("%+v\n", err)
+		}
+	}
+
+	sl, err := store.QueryByBucket(ctx, expectedBucket)
+	if err != nil {
+		t.Fatalf("%+v\n", err)
+	}
+	if e, g := 1, len(sl); e != g {
+		t.Fatalf("expected QueryByBucket results.length %d; got %d", e, g)
+	}
+	if e, g := expectedBucket, sl[0].Bucket; e != g {
+		t.Fatalf("expected Bucket %s; got %s", e, g)
+	}
+}

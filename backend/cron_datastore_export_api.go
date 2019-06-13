a0e3344d83760ee764971d7d80d4983871bbb147
sinmetal <metal.tie@gmail.com>
2019-06-13 14:46:07 +0900

    Datastore ExportのKindsが多い場合、複数のJobに分割するようにした refs #46

--- backend/cron_datastore_export_api.go
+++ backend/cron_datastore_export_api.go
@@ -35,16 +35,37 @@ func (api *CronDatastoreExportAPI) Get(ctx context.Context) error {
 	tq := TQDatastoreExportAPI{}
 	for _, v := range l {
 		// TODO 実行すべきかのハンドリングを追加
-		err := tq.Call(ctx, &TQDatastoreExportAPIPostRequest{
-			ProjectID: v.ProjectID,
-			Bucket:    v.Bucket,
-			Kinds:     v.Kinds,
-		})
-		if err != nil {
-			log.Errorf(ctx, "failed %v, %+v", v.Key, err)
-			return err
+		kindBlocks := buildBlocks(v.Kinds, 30) // Kindの数の上限がいくつなのかは分からないが、成功した値を入れている
+		for _, kinds := range kindBlocks {
+			err := tq.Call(ctx, &TQDatastoreExportAPIPostRequest{
+				ProjectID: v.ProjectID,
+				Bucket:    v.Bucket,
+				Kinds:     kinds,
+			})
+			if err != nil {
+				log.Errorf(ctx, "failed %v, %+v", v.Key, err)
+				return err
+			}
 		}
 	}
 
 	return nil
+}
+
+func buildBlocks(array []string, blockSize int) [][]string {
+	var blockSet [][]string
+	c := len(array) / blockSize
+	if len(array)%blockSize > 0.0 {
+		c++
+	}
+	last := len(array) - 1
+	for i := 0; i < c; i++ {
+		start := (i) * blockSize
+		end := start + blockSize
+		if end > last {
+			end = last
+		}
+		blockSet = append(blockSet, array[start:end])
+	}
+	return blockSet
 }
